06610c069b80da917d923f9309ca189cb8a7dd9b
Salvatore <salvatore.nizza94@gmail.com>
2026-05-11 18:13:35 +0200

    creato avvio software sequenziale

---
+++ Sport Analytics.bat
@@ -0,0 +1,7 @@
+@echo off
+
+start "" "C:\Program Files\DBeaver\dbeaver.exe"
+start "" "C:\Users\salva\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+start "" "C:\Program Files\Tableau\Tableau Public 2026.1\bin\tabpublic.exe"
+
+exit
