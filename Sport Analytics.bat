5ce1bc18921c134312099150353ae863e5679dc0
Salvatore <salvatore.nizza94@gmail.com>
2026-05-11 18:21:53 +0200

    eliminazione avvio software sequenziale

--- Sport Analytics.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-start "" "C:\Program Files\DBeaver\dbeaver.exe"
-start "" "C:\Users\salva\AppData\Local\Programs\Microsoft VS Code\Code.exe"
-start "" "C:\Program Files\Tableau\Tableau Public 2026.1\bin\tabpublic.exe"
-
-exit
